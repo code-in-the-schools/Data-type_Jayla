fd645366db8dd07ba15221f897487b45b29cf8bd
Jayla Nickens <jaylanickensgill11@gmail.com>
2020-07-15 17:17:30 +0000

    .

---
+++ main.py
@@ -0,0 +1,7 @@
+annual_rainfall_2019 = annual_rainfall_2019
+
+janaury_rainfall_2019 = 3.15
+february_rainfall_2019 = 3.64
+march_rainfall_2019 = 4.14
+
+winter_rainfall = janaury_rainfall_2019 + february_rainfall_2019 + march_rainfall_2019 avg_winter_rainfall_2019 = winter_rainfall_2019
